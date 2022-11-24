fef72b384dc80609a049f1b3c6631673578b9b53
Alexander Mata <mata.alexander@seiza-ti.cl>
2022-11-24 12:20:05 -0300

    - Nosotros - Muestra de trabajo imágenes en home

--- src/app/website/components/home/home.component.ts
+++ src/app/website/components/home/home.component.ts
@@ -28,7 +28,36 @@ export class HomeComponent implements OnInit {
       logo: "https://images.squarespace-cdn.com/content/v1/58d15ded6a49638c26e0888c/1553700547770-ZDJL9HYLFIQW463I3FXE/starbucks-coffee.jpg?format=2500w"
     },
 
-  ]
+  ];
+
+  trabajos = [
+    { imagen: 'https://images2.imgbox.com/8c/24/J618LS2j_o.jpeg'},
+    { imagen: 'https://images2.imgbox.com/5d/36/tcM66dAC_o.jpeg'},
+    { imagen: 'https://images2.imgbox.com/60/91/8B1zWr2j_o.jpeg'},
+    { imagen: 'https://images2.imgbox.com/be/3b/6X0Zy9OU_o.jpeg'},
+    { imagen: 'https://images2.imgbox.com/37/64/zPc4hbA0_o.jpeg'},
+    { imagen: 'https://images2.imgbox.com/d1/0b/dAgwne5D_o.jpeg'},
+  ];
+
+  swiperConfig: any = {
+    navigation: true,
+    spaceBetween: 30,
+    autoplay: true,
+    breakpoints: {
+      '992': {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      '480': {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      '640': {
+        slidesPerView: 1,
+        spaceBetween: 50,
+      },
+    },
+  }
   constructor() { }
 
   ngOnInit(): void {
