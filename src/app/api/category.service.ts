107e1ae29ce19a751452dd765e2910b9b7fd6917
Alexander Mata <mata.alexander@seiza-ti.cl>
2022-11-25 14:54:10 -0300

    SweetAlert Angular Material categoryService completo interface para Categories

--- src/app/api/category.service.ts
+++ src/app/api/category.service.ts
@@ -1,25 +1,79 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {RequestCategory, RequestUpdateCategory} from "../interfaces/categories";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
+  private url = `${environment.url}api/categories`;
+
   constructor(
     private httpClient: HttpClient,
   ) { }
 
-  async getCategorias() {
+  async getCategoriesList() {
     try {
       const response: any = await this.httpClient
-        .get(`${environment.url}api/categories/types/posts`)
+        .get(this.url)
         .toPromise();
 
       return response;
     }catch (err) {
       console.log(`Error al obtener información`);
+    }
+  }
+
+  async getCategorias() {
+    try {
+      const response: any = await this.httpClient
+        .get(`${this.url}/types/posts`)
+        .toPromise();
+
+      return response;
+    }catch (err) {
+      console.log(`Error al obtener información`);
+    }
+  }
+
+  async getCategoria(id: string) {
+    try {
+      const response: any = await this.httpClient
+        .get(`${this.url}/${id}`)
+        .toPromise();
+
+      return response;
+    }catch (err) {
+      console.log(`Error al obtener información`);
+    }
+  }
+
+  async create(data: RequestCategory){
+    try {
+      const response: any = await this.httpClient.post(`${this.url}`, data);
+       return response;
+    }catch (e) {
+      console.log(`Error al guardar`);
+    }
+  }
+
+  async update(id:number, data: RequestUpdateCategory){
+    try {
+      const response: any = await this.httpClient.put(`${this.url}`, data);
+      return response;
+    }catch (e) {
+      console.log(`Error al actualizar`);
+    }
+  }
+
+  async delete(idCategory: string){
+    try {
+      const response: any = await this.httpClient.delete(`${this.url}/${idCategory}`);
+      return response;
+    }catch (e) {
+      console.log(`Error al eliminar`);
     }
   }
 }
---
+++ src/app/interfaces/categories.ts
@@ -0,0 +1,19 @@
+export interface RequestCategory {
+  name: string;
+  type: string;
+  slug: string;
+  image: string;
+  description: string;
+}
+
+export interface RequestUpdateCategory {
+  name?: string;
+  type?: string;
+  slug?: string;
+  image?: string;
+  description?: string;
+}
+
+export interface ResponseCategory {
+
+}
