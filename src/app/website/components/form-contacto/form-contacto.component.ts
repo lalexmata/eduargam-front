f79b05ccc1bfa477149f30ee183e9a681efc3304
Alexander Mata <mata.alexander@seiza-ti.cl>
2022-11-26 12:43:52 -0300

    Ajuste direccion en footer depuracion de console.log interfaz de contacto Ajuste formulario de contacto servicio de contacto

--- src/app/website/components/form-contacto/form-contacto.component.ts
+++ src/app/website/components/form-contacto/form-contacto.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from "../../../api/category.service";
-
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ContactoService} from "../../../api/contacto.service";
+import Swall from 'sweetalert2';
 @Component({
   selector: 'app-form-contacto',
   templateUrl: './form-contacto.component.html',
@@ -8,20 +10,89 @@ import {CategoryService} from "../../../api/category.service";
 })
 export class FormContactoComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor(
-    private categoryService: CategoryService
-  ) {}
+    private categoryService: CategoryService,
+    private formBuilder: FormBuilder,
+    private contactoService: ContactoService,
+  ) {
+    this.form = this.buildfForm();
+  }
 
   categorias: any = [];
+
 
 
   ngOnInit(): void {
     this.getCategories();
   }
 
+  private buildfForm() {
+    return this.formBuilder.group({
+      name: [ '', [Validators.required]],
+      rut: [ '', [Validators.required]],
+      telephone: [ '', [Validators.required]],
+      email: [ '', [Validators.required]],
+      address: [ '', [Validators.required]],
+      category_id: [ '', [Validators.required]],
+      tipo_contacto: [ '', [Validators.required]],
+      comments: [ '', [Validators.required]],
+    })
+  }
+
   async getCategories() {
     this.categorias = await this.categoryService.getCategorias();
-    console.log(this.categorias);
+  }
+  get nameField(){
+    return this.form.get('name');
+  }
+  get rutField(){
+    return this.form.get('rut');
+  }
+
+  get telephoneField(){
+    return this.form.get('telephone');
+  }
+  get emailField(){
+    return this.form.get('email');
+  }
+  get addressField(){
+    return this.form.get('address');
+  }
+  get categoryField(){
+    return this.form.get('category_id');
+  }
+  get tipoContactoField(){
+    return this.form.get('tipo_contacto');
+  }
+  get commentsField(){
+    return this.form.get('comments');
+  }
+
+  save(){
+    this.form.markAllAsTouched();
+    console.log(this.form.value);
+    if(!this.form.valid){ return }
+    this.contactoService.create(this.form.value)
+      .then((res) => {
+        console.log('creado', res);
+        Swall.fire({
+          icon: 'success',
+          title: 'Hemos recibido su solicitud',
+          html: 'Nos comunicaremos con usted en la brevedad',
+          confirmButtonText: 'Ok',
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            this.form.reset();
+          }
+        })
+      })
+      .catch((e) => {
+        console.log('Ocurrió un error', e);
+      })
+
   }
 
 }
